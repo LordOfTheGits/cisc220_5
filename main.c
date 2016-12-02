1278e423d5ec70f8ba8eac7ad336f8e9fea8fb67
mattrodgers <15mmr@queensu.ca>
2016-12-01 22:28:22 -0500

    Update main

--- main.c
+++ main.c
@@ -2,37 +2,28 @@
 # include <stdlib.h>
 # include "udll.h"
 
-int main (void)
+int main()
 {
-	union Data d;
-	
-	d.i = 36;
-	insert (0, d);
-	d.i = 16;
-	insert (0, d);
-	d.i = 9;
-	insert (0, d);
-	d.i = 4;
-	insert (0, d);
-	d.i = 2;
-	insert (0, d);
-	d.i = 25;
-	insert (4, d);
-	d.i = 49;
-	insert (6, d);
-	removeNode (3);
-	
-	int len = length ();
-	
-	for(int x = 0; x < len; x++)
-	{
-		union Data dayta = get (x);
-		printf("element %d = %d\n", x, dayta.i);
-	}
+	union Data d0;
+	union Data d1;
+	union Data d2;
 
-	d.c = 'a';
-	insert (0, d);
+	d0.i = 3;
+	d1.f = 6.9;
+	d2.c = 'c';
+	
+	insert(0,d2);
+	insert(0,d1);
+	insert(0,d0);
 
-	d.f = 9.67832;
-	insert (4, d);
+	printf("The length of the list is %d\n", length());
+
+	removeNode (1);
+
+	printf("The length of the list is after a removal of node 1 is %d\n", length());
+	
+	union Data dayta = get(0);
+	printf("The value in the first position is %d\n", dayta.i);
+
+	return 0;
 }
